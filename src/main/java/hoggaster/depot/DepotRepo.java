b7d47e760c28625ced17300d98e7cb0f8d9d650c
svante.kumlien <svante.kumlien@idealo.de>
2015-10-08 12:11:54 +0200

    foobar

--- src/main/java/hoggaster/depot/DepotRepo.java
+++ src/main/java/hoggaster/depot/DepotRepo.java
@@ -2,10 +2,12 @@ package hoggaster.depot;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 /**
  * Created by svante2 on 15-09-20.
  */
 public interface DepotRepo extends MongoRepository<Depot, String> {
 
-    Depot findBybrokerId(String brokerId);
+    Optional<Depot> findByBrokerId(String brokerId);
 }
