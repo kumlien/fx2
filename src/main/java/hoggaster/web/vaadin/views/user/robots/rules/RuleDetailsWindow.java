b8d88378d5be3ced6abb9144fbd02417c429a9e4
svante.kumlien@gmail.com <svante.kumlien@gmail.com>
2016-11-06 22:56:09 +0100

    #43 wip

--- src/main/java/hoggaster/web/vaadin/views/user/robots/rules/RuleDetailsWindow.java
+++ src/main/java/hoggaster/web/vaadin/views/user/robots/rules/RuleDetailsWindow.java
@@ -4,8 +4,8 @@ import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Window;
+import com.vaadin.ui.themes.ValoTheme;
 import hoggaster.rules.Comparator;
-import hoggaster.rules.indicators.Indicator;
 import org.vaadin.viritin.button.MButton;
 import org.vaadin.viritin.fields.MTextField;
 import org.vaadin.viritin.fields.TypedSelect;
@@ -13,11 +13,14 @@ import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
 /**
- * Used to create/edit a rule.
+ * Used to create/edit a single rule.
  *
+ * <p>
  * Created by svante2 on 2016-11-03.
  */
 public class RuleDetailsWindow extends Window {
+
+    private final String[] indicatorNames = new String[]{"Ask", "Bid", "RSI", "Simple value", "SMA"};
 
     public RuleDetailsWindow() {
         super("Create new Rule");
@@ -27,19 +30,31 @@ public class RuleDetailsWindow extends Window {
         content.setMargin(true);
         setContent(content);
 
-        MTextField name = new MTextField("Name:").withNullRepresentation("the name of your rule");
-        name.setNullSettingAllowed(false);
-        content.addComponent(name);
+        MTextField ruleName = new MTextField("Name:").withNullRepresentation("the name of your rule").withFullWidth();
+        ruleName.setNullSettingAllowed(false);
+        content.addComponent(ruleName);
 
-        MHorizontalLayout conditions = new MHorizontalLayout().withSpacing(true);
-        TypedSelect<Indicator> firstIndicator = new TypedSelect<>(Indicator.class).withSelectType(ComboBox.class).withCaption("First indicator").withNullSelectionAllowed(false);
+        MHorizontalLayout conditions = new MHorizontalLayout().withSpacing(true).withFullWidth();
+
+        TypedSelect<String> firstIndicator = new TypedSelect<>(String.class).withSelectType(ComboBox.class).withCaption("First indicator").withNullSelectionAllowed(false).setOptions(indicatorNames);
+        firstIndicator.selectFirst();
+        firstIndicator.addMValueChangeListener(e -> {
+
+        });
+
         TypedSelect<Comparator> comparator = new TypedSelect<>(Comparator.class).withSelectType(ComboBox.class).setOptions(Comparator.values()).withCaption("Comparator").withNullSelectionAllowed(false);
-        TypedSelect<Indicator> secondIndicator = new TypedSelect<>(Indicator.class).withSelectType(ComboBox.class).withCaption("Second indicator").withNullSelectionAllowed(false);
+        comparator.selectFirst();
+
+        TypedSelect<String> secondIndicator = new TypedSelect<>(String.class).withSelectType(ComboBox.class).withCaption("Second indicator").withNullSelectionAllowed(false).setOptions(indicatorNames);
+        secondIndicator.selectFirst();
+
         conditions.add(firstIndicator, comparator, secondIndicator);
+
         content.add(conditions);
 
-        MHorizontalLayout buttons = new MHorizontalLayout().withSpacing(true);
-        Button ok = new MButton("Ok", event -> close()).withIcon(FontAwesome.CHECK_CIRCLE);
+        MHorizontalLayout buttons = new MHorizontalLayout().withSpacing(true).withFullWidth().withFullHeight();
+        Button ok = new MButton("Ok", event -> close()).withIcon(FontAwesome.CHECK_CIRCLE).withStyleName(ValoTheme.BUTTON_PRIMARY);
+
         buttons.add(ok);
         content.add(buttons);
 
